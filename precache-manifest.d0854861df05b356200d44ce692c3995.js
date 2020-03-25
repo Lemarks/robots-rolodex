41190d68ed22a2bd9a1e71da90e1b07bce4bc343
LEMARK-J-MAC <lemark-j-mac@LEMARK-J-MAC.local>
2020-03-25 18:00:07 -0400

    Updates

---
+++ precache-manifest.d0854861df05b356200d44ce692c3995.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1293db251b4b6aaa72e91ec9934f40",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "b7d0e80db10363664bc0",
+    "url": "/robots-rolodex/static/css/main.e7e51795.chunk.css"
+  },
+  {
+    "revision": "391d4da5427670426077",
+    "url": "/robots-rolodex/static/js/2.d3ec30f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-rolodex/static/js/2.d3ec30f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d0e80db10363664bc0",
+    "url": "/robots-rolodex/static/js/main.703f3afd.chunk.js"
+  },
+  {
+    "revision": "b3a69fc78734e45914e8",
+    "url": "/robots-rolodex/static/js/runtime-main.18ab7384.js"
+  }
+]);
